9916194f61007aa6e9445f1857bf40aa6590e036
sycodes95 <sycodes95@gmail.com>
2023-03-08 23:55:51 -0800

    replanned job app table

--- database.sql
+++ database.sql
@@ -1,37 +1,65 @@
 CREATE DATABASE jobtrackr;
 
-CREATE TYPE job_location_type AS ENUM ('On-site', 'Remote', 'Hybrid', 'Both');
+CREATE TYPE job_location_type AS ENUM ('On site', 'Remote', 'Hybrid', 'Both');
 
 
 CREATE TABLE job_app(
   job_app_id SERIAL PRIMARY KEY,
 
-  company_favorite BOOLEAN,
+  company_favorite BOOLEAN, [FILTER]
   company_name VARCHAR(69) NOT NULL UNIQUE,
 
-  job_title VARCHAR(69)
-  job_fit_rating INTEGER, /1/10
-  job_app_date DATE,
-  job_app_method VARCHAR(69), //Company Website, Job Board Website, Recruiter
-  job_source_website VARCHAR(69),
+  job_title VARCHAR(69),
+  job_fit_rating INTEGER, [FILTER]
+  job_app_date DATE, [FILTER]
+  job_app_method VARCHAR(69), //Company Website, Job Board Website, Recruiter, Referral, Other [FILTER]
+  job_location job_location_type, //On site, remote, hybrid, both [FILTER]
+  job_source_website VARCHAR(69), X
 
-  response BOOLEAN,
-  response_date DATE,
-  response_rejected BOOLEAN,
+  response BOOLEAN, [FILTER]
+  response_date DATE, [FILTER] [COMPARE]
+  response_rejected BOOLEAN, [FILTER]
 
-  interview BOOLEAN,
-  interview_date DATE,
-  interview_rejected BOOLEAN,
+  interview BOOLEAN, [FILTER]
+  interview_date DATE, [FILTER] [COMPARE]
+  interview_rejected BOOLEAN, [FILTER]
 
-  offer BOOLEAN,
-  offer_amount INTEGER,
-  job_location job_location_type, //On site, remote, hybrid, both
+  offer BOOLEAN, [FILTER]
+  offer_amount INTEGER, [FITLER] [COMPARE]
+  
 
   contact_person_name VARCHAR(69)
-  contact_person_email VARCHAR()
+  contact_person_email VARCHAR(69)
   contact_person_phone VARCHAR(20)
 
   notes VARCHAR(255)
+
+  user_id INTEGER REFERENCES user_info(user_id);
+);
+
+CREATE TABLE job_app(
+  job_app_id SERIAL PRIMARY KEY,
+  job_app_date DATE, [FILTER]
+  company_favorite BOOLEAN, [FILTER]
+  company_name VARCHAR(69) NOT NULL UNIQUE,
+  company_website VARCHAR(255),
+
+  job_app_method VARCHAR(69), //Company Website, Job Board Website, Recruiter, Referral, Other [FILTER]
+  job_source_website VARCHAR(69), X
+  job_position VARCHAR(69),
+  job_fit_rating INTEGER, [FILTER]
+  job_location job_location_type, //On site, remote, hybrid, both [FILTER]
+  
+  response_date DATE, [FILTER] [COMPARE]
+  interview_date DATE, [FILTER] [COMPARE]
+  offer_amount INTEGER, [FITLER] [COMPARE]
+  rejected VARCHAR,//Res Rejected, Int Rejected [FILTER] 
+
+  contact_person_name VARCHAR(69) [MODAL]
+  contact_person_email VARCHAR(69) [MODAL]
+  contact_person_phone VARCHAR(20) [MODAL]
+
+  notes VARCHAR(255) [MODAL]
 
   user_id INTEGER REFERENCES user_info(user_id);
 );
